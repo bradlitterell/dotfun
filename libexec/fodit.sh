9c7a7a570a8acb2635e00f24270411e034aadafc
Damien Sorresso <dsorresso@fastmail.com>
2024-11-19 21:15:33 -0800

    Fix fodit to properly invoke run_f1.py with the --other-images option and deal with sketchy behavior from Python's argparse. Also add an "elf" image variant to FunOS which is just an alias of the unstripped variant for the benefit of the ISSU tests, and put a hack in fodit to deal with this image type.
    Additionally, construct the tarballs for FunTest and FunSDK with the proper root
    directories.

--- libexec/fodit.sh
+++ libexec/fodit.sh
@@ -3,12 +3,19 @@
 set -e
 SET_MINUS_X='%SET_MINUS_X%'
 if [ -n "$SET_MINUS_X" ]; then
+	echo "executing on host: $(hostname)" >&2
 	set -x
 fi
 
 AR_BASE='%AR_BASE%'
 AR='%AR%'
 FLAVOR='%FLAVOR%'
+
+OTHER_IMAGES=(
+	"FunSDK" "gzip"
+	"FunTest" "gzip"
+	"FunOS" "elf"
+)
 
 # Create a temporary directory and move the archive to it so that it can be
 # easily cleaned up by periodic scripts.
@@ -29,20 +36,54 @@ if [ -z "$image" ]; then
 	exit 1
 fi
 
-# If FunTest is included in the bundle, then make sure we tell that to run_f1.
-if [ -f "images/FunTest/gzip" ]; then
-	tests=$(readlink "images/FunTest/gzip")
-	if [ -n "$tests" ]; then
-		tests=" --other-image $d/images/FunTest/$tests"
-	fi
-fi
+# Collect our supporting cast of images. run_f1 accepts an --other-images
+# option, but Python's argparse is kind of shady with this. It permits stuff to
+# be shortened, and so it recognizes --other-image (singular) as the same
+# option. But it just does a store of the last value it encounters because
+# run_f1 doesn't use argparse's builtin support for collecting multiple
+# instances of a single option together. So we have to assemble the comma-
+# separated list to get the behavior we want; we can't just pass --other-image
+# multiple times.
+set +e
+comma=
+other_images=
+for (( i = 0; i < ${#OTHER_IMAGES[@]}; i += 2 )); do
+	oii=${OTHER_IMAGES[$(( i + 0 ))]}
+	ni=${OTHER_IMAGES[$(( i + 1 ))]}
+	p="images/$oii/$ni"
+	f=
 
-# Same for FunSDK.
-if [ -f "images/FunSDK/gzip" ]; then
-	sdk=$(readlink "images/FunSDK/gzip")
-	if [ -n "$sdk" ]; then
-		sdk=" --other-image $d/images/FunSDK/$sdk"
+	if [ ! -f "$p" ]; then
+		continue
 	fi
+
+	f=$(readlink "$p")
+	if [ ! -n "$f" ]; then
+		continue
+	fi
+
+	# Hack for some tests that expect a stripped, unsigned ELF image to be
+	# present in the job directory with the ".elf" suffix. I'm not sure where
+	# this came from, but it's only in the ISSU tests, and nothing in the FunOS
+	# makefiles appears to produce this. I think the author of those tests
+	# didn't actually understand what each image was and was just trying to
+	# avoid using the signed image, since it had certificates and a header.
+	#
+	# We create a hard link to the target file of the "elf" symlink that has the
+	# appropriate suffix since run_f1.py cannot deal with symlinks properly.
+	if [ "$ni" = "elf" ]; then
+		issu_hack="images/$oii/$f.elf"
+		ln "images/$oii/$f" "$issu_hack"
+		f+=".elf"
+	fi
+
+	other_images+="${comma}${d}/images/$oii/${f}"
+	comma=','
+done
+set -e
+
+if [ -n "$other_images" ]; then
+	other_images=" --other-images $other_images"
 fi
 
 v_arg=
@@ -80,8 +121,7 @@ $whichpip install $v_arg --user requests
 run_f1='~robotpal/bin/run_f1.py'
 run_f1+=" --robot"
 run_f1+=" --params-file test.params"
-run_f1+="$tests"
-run_f1+="$sdk"
+run_f1+="$other_images"
 run_f1+=" $d/images/FunOS/$image"
 
 # The job identifier is written to stderr, so redirect it to stdout for capture
